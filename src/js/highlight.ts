b63f242aa014ccc52cef093604c4bdb8e3298d66
Manuel Grundner <m.grundner@delegate.at>
2020-09-25 17:02:11 +0200

    feat: use prismjs instead of highlight.js

--- src/js/highlight.ts
+++ src/js/highlight.ts
@@ -1,4 +1,15 @@
-import hljs from "@highlightjs/highlight.js";
+import Prism from "prismjs";
 
-hljs.initHighlightingOnLoad();
-hljs.initHighlighting();
+import "prismjs/components/prism-markup-templating.min.js";
+import "prismjs/components/prism-javascript.min.js";
+import "prismjs/components/prism-typescript.min.js";
+import "prismjs/components/prism-csharp.min.js";
+import "prismjs/components/prism-yaml.min.js";
+import "prismjs/components/prism-json.min.js";
+import "prismjs/components/prism-php.min.js";
+import "prismjs/components/prism-sass.min.js";
+import "prismjs/components/prism-css.min.js";
+import "prismjs/components/prism-css-extras.min.js";
+import "prismjs/components/prism-scss.min.js";
+
+Prism.highlightAll();
