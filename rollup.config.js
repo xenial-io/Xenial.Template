2602480c57c125c5accbd71823f078e46073c7d4
Manuel Grundner <m.grundner@delegate.at>
2020-09-26 19:36:59 +0200

    feat: compression and bundleing

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,33 @@
+import fg from "fast-glob";
+
+import { brotliCompressSync } from "zlib";
+
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import scss from "rollup-plugin-scss";
 import copy from "rollup-plugin-copy";
+import { terser } from "rollup-plugin-terser";
+import gzipPlugin from "rollup-plugin-gzip";
+import filesize from "rollup-plugin-filesize";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
+const additionalFiles = [
+  "./dist/css/main.css",
+  ...fg.sync("./dist/css/*.svg"),
+  ...fg.sync("./dist/css/*.ttf"),
+  ...fg.sync("./dist/img/*.svg"),
+  ...fg.sync("./dist/img/*.png"),
+  ...fg.sync("./dist/img/*.ico"),
+];
+
 export default [
   {
     input: "src/index.ts",
     output: {
       file: "dist/index.min.js",
       format: "iife",
+      plugins: [terser()],
     },
     external: [],
     plugins: [
@@ -22,6 +39,7 @@ export default [
       }),
       scss({
         output: "dist/css/main.css",
+        outputStyle: "compressed",
       }),
       copy({
         targets: [
@@ -32,6 +50,16 @@ export default [
           { src: "src/img/*.*", dest: "./dist/img" },
         ],
       }),
+      gzipPlugin({
+        additionalFiles,
+      }),
+      gzipPlugin({
+        additionalFiles,
+        customCompression: (content) =>
+          brotliCompressSync(Buffer.from(content)),
+        fileName: ".br",
+      }),
+      filesize(),
     ],
     //   sourceMap: "inline",
   },
