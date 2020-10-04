0475dbb76ec34ad34df44ac756f61209bac9661f
Manuel Grundner <m.grundner@delegate.at>
2020-10-04 21:22:59 +0200

    feat: export store

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import "./css/main.scss";
 import { themeing } from "./js/theme";
 import { highlight } from "./js/highlight";
-
+import { store } from "./js/store";
 const xenial = () => {
   themeing();
   highlight();
 };
 
-export { xenial };
+export { xenial, store };
