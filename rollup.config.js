e14b0f50c85202279df8dbdd30b1bf8c99026d20
Manuel Grundner <m.grundner@delegate.at>
2020-10-07 14:03:24 +0200

    build: remove unused packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,18 @@
 import pkg from "./package.json";
 
-import fg from "fast-glob";
-
-import { brotliCompressSync } from "zlib";
-
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import scss from "rollup-plugin-scss";
 import copy from "rollup-plugin-copy";
-import { terser } from "rollup-plugin-terser";
-import gzipPlugin from "rollup-plugin-gzip";
 import filesize from "rollup-plugin-filesize";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
-const additionalFiles = [
-  "./dist/css/main.css",
-  ...fg.sync("./dist/css/*.svg"),
-  ...fg.sync("./dist/css/*.ttf"),
-  ...fg.sync("./dist/img/*.svg"),
-  ...fg.sync("./dist/img/*.png"),
-  ...fg.sync("./dist/img/*.ico"),
-];
 
 export default [
   {
     input: "src/index.ts",
     output: [
-      // {
-      //   name: "xenial-template",
-      //   file: pkg.browser,
-      //   format: "umd",
-      //   plugins: [terser(), resolve(), commonjs()],
-      // },
-      // { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" },
     ],
     external: [],
@@ -57,15 +36,6 @@ export default [
           { src: "src/img/*.*", dest: "./dist/img" },
         ],
       }),
-      // gzipPlugin({
-      //   additionalFiles,
-      // }),
-      // gzipPlugin({
-      //   additionalFiles,
-      //   customCompression: (content) =>
-      //     brotliCompressSync(Buffer.from(content)),
-      //   fileName: ".br",
-      // }),
       filesize(),
     ],
   },
